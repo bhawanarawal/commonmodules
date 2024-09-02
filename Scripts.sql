f52eb3b345456b7da81f3a527e213c31775235b3
bhawana rawal <bhwbi.rawal@gmail.com>
2024-09-02 10:24:04 +0545

    add tables and data

---
+++ Scripts.sql
@@ -0,0 +1,63 @@
+-- SQLite
+--drop table AuthorDetails;
+--drop table Authors;
+--drop table Books;
+--drop table Publishers;
+
+--Tables for bookstore
+--Book (Id(PK),Title,PublishedDate,Price,Edition,ISBN,Genre,PublicationId(FK),AuthorId(FK))
+--Publication(Id,Name,Address,Established)
+--Author(Id,Name,Address,Phone,Gender,Education)
+
+--one to one , one to many , many to many
+
+CREATE table Author
+(
+  Id Integer primary key autoincrement,
+  Name text,
+  Address text,
+  Phone text,
+  Gender text,
+  Education text
+
+);
+
+select * from Author
+
+insert into Author 
+values(null,'yaswant kenetkar','ktm','985670456','M','bca');
+insert into Author 
+values(null,'napier','pokhara','92340456','M','Mit');
+insert into Author 
+values(null,'Augusta','surkhet','904567','F','Mba');
+
+create table publication
+(
+    Id Integer primary key autoincrement,
+    Name text,
+    Address text,
+    Established integer
+    );
+insert into Publication 
+values(null,'james','ktm','2060');
+insert into Publication 
+values(null,'Pascal','pokhara','2075');
+insert into Publication 
+values(null,'julie','Ilam','2040');
+create table Book
+(
+    Id Integer primary key autoincrement,
+    Title text,
+    PublishedDate Integer,
+    Price integer,
+    Edition text,
+    ISBN text,
+    Genre text,
+    publicationId integer,
+    AuthorId integer,
+    FOREIGN KEY (publicationId) REFERENCES publication(publicationId),
+    FOREIGN KEY (AuthorId) REFERENCES Author(AuthorId)
+    );
+
+    insert into Book 
+    VALUES(null,'Data structure and algorithm',2045 , 600 , 'II','987-0-1-04','science and technology',1,1);
